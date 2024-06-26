f129aa6cac445266ce8b0cb296e4fc53762aa2bc
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-06-26 09:00:54 +0100

    changed hero image

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -6,7 +6,7 @@
           <div aria-hidden="true" class="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
           <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
             <div class="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-              
+
               <img class="object-cover lg:h-full lg:w-full" :src="testimony.image" alt="" />
             </div>
           </div>
