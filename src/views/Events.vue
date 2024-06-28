c56ea76bd5361a709ad8127a87db9d5109712241
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-06-28 09:23:50 +0100

    spacing

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
-  <div class="mx-auto pt-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:pt-24">
+  <div class="mx-auto pt-12 pb-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:pt-24">
     <h2 class="text-center lg:text-left text-3xl font-extrabold text-gray-900 sm:text-5xl  lg:text-6xl font-serif tracking-normal">Events</h2>
 
   <div class="mt-12 space-y-12 lg:space-y-0 grid grid-cols-1 gap-5 lg:gap-x-8">
       <div v-for="event in events" :key="event.title" class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
         <div class="grid grid-cols-1 lg:grid-cols-2">
           <img class="h-full md:h-96" :src="event.image"/>
-          <div>
+          <div class="mt-5 md:mt-0">
             <h3 class="text-3xl font-semibold text-gray-900 font-serif tracking-normal">{{ event.title }}</h3>
             <h4 class="mt-4 text-xl font-semibold text-yellow-300 font-serif tracking-normal">{{ event.date }}</h4>
             <p class="mt-4 flex items-baseline text-gray-900">
