f2f97c4b2f86d7e42a355660c30959767e443891
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-06-28 09:27:12 +0100

    updated venue

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -9,8 +9,10 @@
           <img class="h-full md:h-96" :src="event.image"/>
           <div class="mt-5 md:mt-0">
             <h3 class="text-3xl font-semibold text-gray-900 font-serif tracking-normal">{{ event.title }}</h3>
-            <h4 class="mt-4 text-xl font-semibold text-yellow-300 font-serif tracking-normal">{{ event.date }}</h4>
-            <p class="mt-4 flex items-baseline text-gray-500">{{ event.venue }}</p>
+            <div class="flex">
+              <h4 class="mt-4 mr-4 text-xl font-semibold text-yellow-300 font-serif tracking-normal">{{ event.date }}</h4>
+              <p class="mt-4 flex items-baseline text-gray-500">- {{ event.venue }}</p>
+            </div>
             <p
               v-for="description in event.description" :key="description"
               class="mt-2 text-gray-500"
@@ -33,7 +35,7 @@ const events = [
   {
     title: 'Beauty and the Beast Jr.',
     date: 'June 2025',
-    venue: 'TBC',
+    venue: 'Venue TBC',
     image: '/images/beauty-and-the-beast-jr.png',
     tags: [
       'Titans',
