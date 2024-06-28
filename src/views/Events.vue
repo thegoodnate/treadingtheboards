fff9de72a047da465a1118882cb79d3169fdeb3e
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-06-28 11:00:45 +0100

    contact link

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -31,6 +31,11 @@
               >
                 {{ tag }}
             </span>
+
+            <div v-if="event.upcoming" class="mt-8">
+              <router-link to="/contact" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                Contact us
+              </router-link>
             </div>
           </div>
         </div>
