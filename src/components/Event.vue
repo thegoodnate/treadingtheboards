fc037ac14674b68b5ff4635d9878ba078c2bbfac
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-07-18 12:25:00 +0100

    fixed href

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -28,7 +28,7 @@
         </span>
       </div>
       <div v-if="event.upcoming" class="mt-8">
-        <a thref="/beauty_and_the_beast/Audition_Pack.pdf" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+        <a href="/beauty_and_the_beast/Audition_Pack.pdf" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
           Download Audition Pack
         </a>
       </div>
