ff70c758cc136d9fa27894a2550cc9942f3182a9
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-09-25 13:25:36 +0100

    moving text

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -9,22 +9,22 @@
   <div class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
     <div class="flex flex-wrap justify-between">
       <div class="flex flex-col text-center">
-        <img class="md:h-96 mb-2 md:mb-0 mr-0" src="/images/bee-musical.png"/>
-        <p class="mb-5 md:mb-0 text-2xl leading-8 font-extrabold tracking-normal text-yellow-300 sm:text-3xl font-serif">
+        <p class="md:mb-0 text-2xl leading-8 font-extrabold tracking-normal text-yellow-300 sm:text-3xl font-serif">
           March
         </p>
+        <img class="md:h-96 mt-2 mb-5 md:mb-0" src="/images/bee-musical.png"/>
       </div>
       <div class="flex flex-col text-center">
-        <img class="md:h-96 mb-2 md:mb-0 mr-0" src="/images/beauty-and-the-beast-jr.png"/>
-        <p class="mb-5 md:mb-0 text-2xl leading-8 font-extrabold tracking-normal text-yellow-300 sm:text-3xl font-serif">
+        <p class="md:mb-0 text-2xl leading-8 font-extrabold tracking-normal text-yellow-300 sm:text-3xl font-serif">
           June
         </p>
+        <img class="md:h-96 mt-2 mb-5 md:mb-0" src="/images/beauty-and-the-beast-jr.png"/>
       </div>
       <div class="flex flex-col text-center">
-        <img class="md:h-96 mb-2 md:mb-0 mr-0" src="/images/wedding-singer.png"/>
         <p class="text-2xl leading-8 font-extrabold tracking-normal text-yellow-300 sm:text-3xl font-serif ">
           July
         </p>
+        <img class="md:h-96 mt-2 mb-5 md:mb-0" src="/images/wedding-singer.png"/>
       </div>
     </div>
   </div>
