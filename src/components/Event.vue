240f2b6dc3cc5b1aec5f1b5acecb02b6c512331a
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-09-25 10:38:36 +0100

    proper spacing

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -3,7 +3,7 @@
   <div class="absolute font-serif tracking-normal font-semibold top-0 right-0 bg-yellow-300 text-black px-4 py-2 rounded-bl-lg rounded-tr-lg">
     Upcoming Events
   </div>
-  <div class="flex flex-wrap">
+  <div class="flex flex-wrap justify-between">
     <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/bee-musical.png"/>
     <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/beauty-and-the-beast-jr.png"/>
     <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/wedding-singer.png"/>
