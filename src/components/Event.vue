10471ccbcb7bf307e51e03ee08e1c9d4f1ba8c91
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-09-25 11:42:22 +0100

    update to events splash

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -8,9 +8,24 @@
 
   <div class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
     <div class="flex flex-wrap justify-between">
-      <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/bee-musical.png"/>
-      <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/beauty-and-the-beast-jr.png"/>
-      <img class="md:h-96 md:mb-0 mr-0" src="/images/wedding-singer.png"/>
+      <div class="flex flex-col text-center">
+        <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/bee-musical.png"/>
+        <p class="mt-2 text-2xl leading-8 font-extrabold tracking-normal text-gray-900 sm:text-4xl font-serif">
+          March 2025
+        </p>
+      </div>
+      <div class="flex flex-col text-center">
+        <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/beauty-and-the-beast-jr.png"/>
+        <p class="mt-2 text-2xl leading-8 font-extrabold tracking-normal text-gray-900 sm:text-4xl font-serif">
+          June 2025
+        </p>
+      </div>
+      <div class="flex flex-col text-center">
+        <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/wedding-singer.png"/>
+        <p class="mt-2 text-2xl leading-8 font-extrabold tracking-normal text-gray-900 sm:text-4xl font-serif">
+          July 2025
+        </p>
+      </div>
     </div>
   </div>
 </div>
