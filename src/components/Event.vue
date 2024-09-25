d46d4f2f2e1b4cedd470ca9292d62c4fcea13225
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2024-09-25 10:41:40 +0100

    changes

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,12 +1,17 @@
 <template>
-<div class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
-  <div class="absolute font-serif tracking-normal font-semibold top-0 right-0 bg-yellow-300 text-black px-4 py-2 rounded-bl-lg rounded-tr-lg">
-    Upcoming Events
+<div>
+  <div class="text-center">
+    <p class="mt-2 text-3xl leading-8 font-extrabold tracking-normal text-gray-900 sm:text-4xl font-serif">
+      Upcoming Events
+    </p>
   </div>
-  <div class="flex flex-wrap justify-between">
-    <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/bee-musical.png"/>
-    <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/beauty-and-the-beast-jr.png"/>
-    <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/wedding-singer.png"/>
+
+  <div class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+    <div class="flex flex-wrap justify-between">
+      <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/bee-musical.png"/>
+      <img class="md:h-96 mb-5 md:mb-0 mr-0" src="/images/beauty-and-the-beast-jr.png"/>
+      <img class="md:h-96 md:mb-0 mr-0" src="/images/wedding-singer.png"/>
+    </div>
   </div>
 </div>
 </template>
