3a62fcbcbe632d54509ccbe9c7601c9539ce7084
Jones, Nathan G <Nathan.G.Jones@lexisnexisrisk.com>
2025-01-31 09:31:38 +0000

    updated book tickets link

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -28,5 +28,12 @@
       </div>
     </div>
   </div>
+
+  <div class="text-center mt-8">
+    <a href="https://www.ticketsource.co.uk/treading-the-boards" target="_blank"
+      class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+      Book Tickets
+    </a>
+  </div>
 </div>
 </template>
